39640dbcb2526be88641b6e72bf3ad0b001a2e35
jonatansalas <jonatansalas@live.com.ar>
2017-02-22 14:04:48 -0300

    Trying to fix indentation

--- src/lib/GridFsStorage.js
+++ src/lib/GridFsStorage.js
@@ -8,14 +8,14 @@ let gridFs;
 connection.once('open', () => gridFs = GridFs(connection.db, mongoose.mongo));
 
 class GridFsStorage {
-	constructor(options = {}) {
-		this.getDestination = defaultOptions.getDestination;
-		this.getFileName = options.getFilename || defaultOptions.getFilename;
-		this.streamOptions = options.streamOptions || defaultOptions.streamOptions;
-	}
+    constructor(options = {}) {
+        this.getDestination = defaultOptions.getDestination;
+        this.getFileName = options.getFilename || defaultOptions.getFilename;
+        this.streamOptions = options.streamOptions || defaultOptions.streamOptions;
+    }
 
-	_handleFile = (request, file, callback) => {
-		this.getDestination(request, file).then(() => {
+    _handleFile = (request, file, callback) => {
+        this.getDestination(request, file).then(() => {
             this.getFileName(request, file).then(filename => {
                 const outStream = gridFs.createWriteStream({
                     ...this.streamOptions,
@@ -29,16 +29,16 @@ class GridFsStorage {
                     .on('close', file => callback(null, file));
             });
         });
-	};
+    };
 	
 	_removeFile = (request, file, callback) => {
-        gridFs.exist({_id: file._id}, (error, found) => {
-            if (error) callback(error);
-				
-            if (found) {
-                gridFs.remove({_id: file._id}, error => error ? callback(error) : callback(null, file._id));
-            }
-        });
+	    gridFs.exist({_id: file._id}, (error, found) => {
+	        if (error) callback(error);
+
+	        if (found) {
+	            gridFs.remove({_id: file._id}, error => error ? callback(error) : callback(null, file._id));
+	        }
+	    });
 	};
 }
 
